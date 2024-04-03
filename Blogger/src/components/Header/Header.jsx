ac2bd4b0e982abba8f7962f24325e2f4ddde8f9a
Gunjankher <gkhercode@gmail.com>
2024-04-03 14:21:40 +0530

    Header complete

--- Blogger/src/components/Header/Header.jsx
+++ Blogger/src/components/Header/Header.jsx
@@ -4,3 +4,90 @@
 // usenavigate 
 // make a array of the header items of slug name and active 
 // check the each componenrs with authstatus whether it is active or not
+
+
+import React from 'react'
+import {LogoutBtn, Logo, Container} from '../index'
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+
+function Header() {
+
+const navigate = useNavigate()
+const authStatus = useSelector((state)=> state.auth.status)
+
+
+const navItems = [
+{
+name: "Home",
+slug:"/",
+active : true
+},
+
+{
+name : "Login",
+slug :"/login",
+active :!authStatus
+
+},
+
+{
+    name :"Signup",
+    slug :"/singup",
+    active : !authStatus
+},
+
+{
+    name : "All Posts",
+    slug :"/all-posts",
+    active : authStatus
+},
+
+
+{
+    name: "Add Post",
+    slug: "/add-post",
+    active : authStatus
+}
+
+]
+
+
+
+
+  return (
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px'   />
+
+              </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+        </Container>
+    </header>
+  )
+}
+
+export default Header
---
+++ Blogger/src/components/index.js
@@ -0,0 +1,12 @@
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Container from './container/Container'
+import Header from "./Header/Header";
+
+
+export {
+    LogoutBtn,
+    Logo,
+    Container,
+    Header,
+}
