4ff737265caa2a7cdbfae1185e2a4da354df76b1
Gunjankher <gkhercode@gmail.com>
2024-04-03 13:44:35 +0530

    Header

--- Blogger/src/components/Header/Header.jsx
+++ Blogger/src/components/Header/Header.jsx
@@ -1,1 +1,6 @@
-
+// make a header Component
+// design thev algo way that the header page have a diffrent when login and logout
+// bring the status from the store using the useSelector 
+// usenavigate 
+// make a array of the header items of slug name and active 
+// check the each componenrs with authstatus whether it is active or not
