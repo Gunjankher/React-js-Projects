844cc36f6112034ac7d6bf1545f02822ab24108d
Gunjankher <gkhercode@gmail.com>
2024-04-05 14:39:02 +0530

    setup routing

--- Blogger/src/main.jsx
+++ Blogger/src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import {RouterProvider , createBrowserRouter} from 'react-router-dom'
+import { AuthLayout, Login } from './components/index.js'
+
+
+// pages 
+
+import Home from './components/pages/Home.jsx'
+import AddPost from './components/pages/AddPost.jsx'
+import Signup from './components/pages/Signup.jsx'
+import EditPost from './components/pages/EditPost.jsx'
+import Post from './components/pages/Post.jsx'
+import AllPosts from './components/pages/AllPost.jsx'
+
+
+const router = createBrowserRouter([
+
+{
+  path :"/",
+  element:<App />,
+  children:[
+
+
+
+
+
+
+
+
+  ]
+}
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+<Provider store = {store}>
+<RouterProvider router = {router} />
+</Provider>
   </React.StrictMode>,
 )
