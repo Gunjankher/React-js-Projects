07204aaf84d7f27ac44c12c6a6faa629912c0549
Gunjankher <gkhercode@gmail.com>
2024-04-13 13:14:27 +0530

    hompage

--- E-Commerce App/src/components/Homepage.jsx
+++ E-Commerce App/src/components/Homepage.jsx
@@ -12,11 +12,30 @@ Homepage
 <Carousel />
 <div className='grid grid-cols-3 xl:grid-cols-4'>
 
+
 <HomepageCard
 title={"Appliances for your home | Up to 55% off"}
 name1 = {"Air Conditior"}
 img = {'../Images/homecard_1.jpg'}
+name2={"hi"}
+img3 = {'../Images/homecard_2.jpg'}
+name3={"hi"}
+img4 = {'../Images/homecard_3.jpg'}
+name4={"hi"}
+img2 = {'../Images/homecard_4.jpg'}
 />
+<HomepageCard
+title={"Appliances for your home | Up to 55% off"}
+name1 = {"Air Conditior"}
+img = {'../Images/homecard_1.jpg'}
+name2={"hi"}
+img3 = {'../Images/homecard_2.jpg'}
+name3={"hi"}
+img4 = {'../Images/homecard_3.jpg'}
+name4={"hi"}
+img2 = {'../Images/homecard_4.jpg'}
+/>
+
 
 <div>3</div>
 <div>4</div>
--- E-Commerce App/src/components/HomepageCard.jsx
+++ E-Commerce App/src/components/HomepageCard.jsx
@@ -1,38 +1,73 @@
-import React from 'react'
+// import React from 'react'
 
-function HomepageCard({
-    title,
-    img,
-    name1,
-    name2,
-    link
-}) {
+// function HomepageCard({
+//     title,
+//     img,
+//     name1,
+//     name2,
+//     link
+// }) {
 
 
+//   return (
+//     <div className='h-[420px] bg-white '>
+// <div className='text-xl block'>
+
+//     {title}
+// </div>
+
+// <div className=' flex '>
+
+// <div className='m-4'>
+// <img className='w-[140px] h-[120px] object-cover' src={img}  />
+// <div className='text-[12px]'>{name1}</div>
+// </div>
+
+// </div>
+
+
+
+//     </div>
+//   )
+// }
+
+// export default HomepageCard
+
+import React from 'react';
+
+function HomepageCard({ title, img,img2, img3,img4, name1, name2, name3,name4, link }) {
   return (
-    <div className='h-[420px] bg-white '>
+    <div className='h-[420px] bg-white'>
+      {title && <div className='text-xl block mx-4 font-semibold '>{title}</div>}
 
-<div className='text-xl'>
-    {title}
-</div>
+      <div className='flex'>
+        <div className='m-4'>
+          <img className='w-[140px] h-[120px] object-cover' src={img} alt='Image 1' />
+          <div className='text-[12px]'>{name1}</div>
+        </div>
+        {/* Repeat this block for the other images */}
 
-<div className=' flex '>
+        <div className='m-4'>
+          <img className='w-[140px] h-[120px] object-cover' src={img2} alt='Image 2' />
+          <div className='text-[12px]'>{name2}</div>
+        </div>
+        
+      </div>
+      <div className='flex'>
+        <div className='m-4'>
+          <img className='w-[140px] h-[120px] object-cover' src={img3} alt='Image 1' />
+          <div className='text-[12px]'>{name3}</div>
+        </div>
+        {/* Repeat this block for the other images */}
 
-<div className='m-4'>
-<img className='w-[140px] h-[120px] object-cover' src={img}  />
-<div className='text-[12px]'>{name1}</div>
-</div>
-
-{/* <div className='m-4'>
-<img className='w-[140px] h-[120px] object-cover' src={img}  />
-<div className='text-[12px]'>{name2}</div>
-</div> */}
-</div>
-
-
-
+        <div className='m-4'>
+          <img className='w-[140px] h-[120px] object-cover' src={img4} alt='Image 2' />
+          <div className='text-[12px]'>{name4}</div>
+        </div>
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomepageCard
+export default HomepageCard;
