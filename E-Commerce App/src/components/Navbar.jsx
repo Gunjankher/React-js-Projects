c883704ec3c079a4b40c5158d435fb6e1b41994b
Gunjankher <gkhercode@gmail.com>
2024-04-16 13:24:53 +0530

    Search bar component

--- E-Commerce App/src/components/Navbar.jsx
+++ E-Commerce App/src/components/Navbar.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import {ShoppingCartIcon} from '@heroicons/react/24/outline'
+import Search from './Search'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='m-w-[1000px]'>
+       <div className='flex bg-amazonclone text-white h-[60px]'>
+{/* left */}
+<div className='flex items-center m-4'>
+    <img className='h-[35px] w-[100px] m-2' src="Images/amazon.png"/>
+  <div className='pr-4 pl-3'>
+<div className='text-xs xl:text-sm'>Deliver to</div>
+<div className='text-sm xl:text-base font-bold'>Ahmedabad</div>
+    </div>  
+</div>
+
+{/* middle */}
+<div className='flex reletive items-center grow' >
+    <Search />
+</div>
+{/* right */}
+
+
+<div className='flex items-center m-4'>
+<div className='pr-4 pl-3'>
+<div className='text-xs xl:text-sm'>Hello</div>
+<div className='text-sm xl:text-base font-bold'>Sign in </div>
+    </div>  
+    <div className='pr-4 pl-3'>
+<div className='text-xs xl:text-sm'>Return</div>
+<div className='text-sm xl:text-base font-bold'>&Orders</div>
+    </div>  
+
+<div className='pl-3 pr-3 flex'>
+<ShoppingCartIcon className='h-[48px]' />
+<div className=' mt-7 text-xs xl:text-sm font-bold '>
+    Cart
+</div>
+</div>
+
+</div>
+
+       </div>
+    </div>
   )
 }
 
