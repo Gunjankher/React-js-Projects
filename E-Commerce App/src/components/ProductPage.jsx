2a266362af3381dbdecf176e99a2b882137717a5
Gunjankher <gkhercode@gmail.com>
2024-04-17 13:49:47 +0530

    ..........

--- E-Commerce App/src/components/ProductPage.jsx
+++ E-Commerce App/src/components/ProductPage.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState , useEffect} from 'react'
 import {useParams}from 'react-router-dom'
+import { callAPI } from '../utlis/CallApi'
 
 function ProductPage() {
+    const[product, setProduct] = useState(null)
     const{id} = useParams()
+
+    const getProduct = ()=>{
+        callAPI(`data/products.json`)
+        .then((ProducutResult)=>{
+            setProduct(ProducutResult[id])
+        })
+    }
+
+    useEffect(()=>{
+getProduct()
+    },[])
+if(!product?.title)return <h1>loading....</h1>
+
   return (
-    <div>ProductPage{id}</div>
+    <div className='min-w-[1000px] max-w-[1500px] m-auto bg-orange-400 '>
+<div className='grid grid-cols-10'>
+  {/* left */}
+<div className='col-span-3 bg-blue-400'>
+hi
+</div>
+{/* middle */}
+<div className='col-span-5 bg-pink-500'>
+hi
+</div>
+{/* right */}
+<div className='col-span-2 bg-yellow-400'>
+  hi
+</div>
+
+</div>
+    </div>
   )
 }
 
