efdb0fe27f6f1b51c8fe94460e1e5436a7b7adfb
Gunjankher <gkhercode@gmail.com>
2024-04-17 15:58:15 +0530

    .......

--- E-Commerce App/src/components/ProductPage.jsx
+++ E-Commerce App/src/components/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState , useEffect} from 'react'
 import {useParams}from 'react-router-dom'
 import { callAPI } from '../utlis/CallApi'
+import ProductDetails from './ProductDetails'
 
 function ProductPage() {
     const[product, setProduct] = useState(null)
@@ -8,8 +9,8 @@ function ProductPage() {
 
     const getProduct = ()=>{
         callAPI(`data/products.json`)
-        .then((ProducutResult)=>{
-            setProduct(ProducutResult[id])
+        .then((ProductResult)=>{
+            setProduct(ProductResult[id])
         })
     }
 
@@ -21,13 +22,16 @@ if(!product?.title)return <h1>loading....</h1>
   return (
     <div className='h-screen bg-amazonclone-background'>
     <div className='min-w-[1000px] max-w-[1500px] m-auto bg-orange-400 '>
-<div className='grid grid-cols-10'>
-<div className='col-span-3 bg-blue-400 p-8 '>
-<img src={`${product.title}`} />
+<div className='grid grid-cols-10 gap-2'>
+<div className='col-span-3 bg-white p-8 m-auto'>
+<img src={`${product.image}`} />
 </div>
 {/* middle */}
-<div className='col-span-5 bg-pink-500'>
-hi
+<div className='col-span-5 p-4 rounded bg-white divide-y divide-gray-500'>
+<div>
+<ProductDetails product={product} ratings={true} />
+</div>
+<div></div>
 </div>
 {/* right */}
 <div className='col-span-2 bg-yellow-400'>
