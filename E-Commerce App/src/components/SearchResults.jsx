cd10d46246921f88486b03366bca45a01e254b74
Gunjankher <gkhercode@gmail.com>
2024-04-18 17:04:26 +0530

    ............

--- E-Commerce App/src/components/SearchResults.jsx
+++ E-Commerce App/src/components/SearchResults.jsx
@@ -1,36 +1,68 @@
 import React, { useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams ,Link } from 'react-router-dom'
 import { useEffect } from 'react'
 import { callAPI } from '../utlis/CallApi'
 
 
 
 
-function SearchResults() {
-  const [searchParams] = useSearchParams()
-  const [products , setProducts] = useState(null)
 
-const getSearchResults = ()=>{
-  const searchTerm  = searchParams.get("searchTerm")
-  const catogary = searchParams.get("category")
-callAPI(`data/search.json`)
-.then((SearchResults)=>{
-const categoryResults = SearchResults[catogary]
-if(searchTerm){
-const results = categoryResults.filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()))
-setProducts(results)
-}
+// function SearchResults() {
+//   const [searchParams] = useSearchParams()
+//   const [products , setProducts] = useState(null)
 
-})
+// const getSearchResults = ()=>{
+//   const searchTerm  = searchParams.get("searchTerm")
+//   const category = searchParams.get("category")
+// callAPI(`data/search.json`)
+// .then((SearchResults)=>{
+// const categoryResults = SearchResults[category]
+// if(searchTerm){
+// const results = categoryResults.filter((product) => product.title.toLowerCase().includes(searchTerm.toLowerCase()))
+// setProducts(results)
+// }else{
+//   setProducts(categoryResults)
+// }
 
-}
+// })
+
+// }
+const SearchResults = () => {
+  const [searchParams] = useSearchParams();
+  const [products, setProducts] = useState(null);
+
+  const getSearchResults = () => {
+    const searchTerm = searchParams.get("searchTerm");
+    const category = searchParams.get("category");
+
+    callAPI(`data/search.json`).then((searchResults) => {
+      const categoryResults = searchResults[category];
+      if (searchTerm) {
+        const results = categoryResults.filter((product) =>
+          product.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setProducts(results);
+      } else {
+        setProducts(categoryResults);
+      }
+    });
+  };
 
 
 useEffect(()=>{
 getSearchResults()
 },[searchParams])
+
   return (
-    <div>SearchResults</div>
+    <div className="min-w-[1200px] max-w-[1300px] m-auto pt-4">
+   {products && products.map((product , key)=>{
+    return (
+      <div key={key}>
+        {product.title}
+      </div>
+    )
+   })}
+  </div>
   )
 }
 
