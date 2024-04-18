c63ae54138d0d0ff170855feba36c9aef1b502f7
Gunjankher <gkhercode@gmail.com>
2024-04-18 16:47:55 +0530

    ................

--- E-Commerce App/src/components/Search.jsx
+++ E-Commerce App/src/components/Search.jsx
@@ -1,30 +1,88 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { callAPI } from '../utlis/CallApi'
+import { useNavigate, createSearchParams } from 'react-router-dom'
 
 function Search() {
 
-const [suggestion , setSuggestion] = useEffect(null)
+const [suggestion , setSuggestion] = useState(null)
+const [searchTerm , SetsearchTerm] = useState("")
+const [catogary , setCatogarty] = useState("")
+const navigate = useNavigate()
+
+const onHandleSubmit = (e)=>{
+e.preventDefault()
+
+navigate({
+
+  pathname : "search",
+  search : `${
+    createSearchParams({
+
+catogary : `${catogary}`,
+searchTerm : `${searchTerm}`,
+
+    })
+  }`
+})
+setCatogarty("")
+setCatogarty("All")
+}
+
+const getSuggestion = () => { 
+callAPI(`data/suggestion.json`)
+.then((suggestionResults)=>{
+  setSuggestion(suggestionResults)
+})
+
+}
+
+useEffect(()=>{
+getSuggestion()
+},[])
 
   return (
     <div className=' w-[100%]'>
         <div className='flex items-center h-10 bg-amazonclone-yellow rounded'>
-        <select className='p-2 bg-gray-300 text-black border text-xs xl:text-sm'>
+        <select onChange={(e)=>setCatogarty(e.target.value) }
+        className='p-2 bg-gray-300 text-black border text-xs xl:text-sm'>
             
-<option>All</option>
-<option>2</option>
-<option>3</option>
-<option>4</option>
-<option>5</option>
-<option>6</option>
+        <option>All</option>
+          <option>Deals</option>
+          <option>Amazon</option>
+          <option>Fashion</option>
+          <option>Computers</option>
+          <option>Home</option>
+          <option>Mobiles</option>
         </select>
-            <input className='flex grow items-center h-[100%] rounded-l text-black' type='text ' />
-            <button className='w-[45px]'>
+            <input className='flex grow items-center h-[100%] rounded-l text-black' type='text  ' value={searchTerm}
+            
+            onChange={(e)=>SetsearchTerm(e.target.value)}
+            />
+            <button onChange={onHandleSubmit}
+             className='w-[45px]'>
                 <MagnifyingGlassIcon className='h-[27px] m-auto stroke-slate-900' />
             </button>
         </div>
         {suggestion &&
-        <div>
-          
+        <div className='bg-white tesxt-black w-full z-40 absolute text-black'>
+{
+  suggestion.filter((suggestion)=>{
+    const currentsearchTerm = searchTerm.toLowerCase()
+    const title = suggestion.title.toLowerCase()
+return(
+  currentsearchTerm && 
+  title.startsWith(currentsearchTerm)&&
+  title !== currentsearchTerm
+)
+  })
+  .slice(0,10)
+  .map((suggestion)=>(
+    <div key={suggestion.id} onClick={()=>SetsearchTerm(suggestion.title)}>
+      {suggestion.title}
+    </div>
+  ))
+}
         </div>
         }
     </div>
--- E-Commerce App/src/components/SearchResults.jsx
+++ E-Commerce App/src/components/SearchResults.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { callAPI } from '../utlis/CallApi'
+
+
+
 
 function SearchResults() {
+  const [searchParams] = useSearchParams()
+  const [products , setProducts] = useState(null)
+
+const getSearchResults = ()=>{
+  const searchTerm  = searchParams.get("searchTerm")
+  const catogary = searchParams.get("category")
+callAPI(`data/search.json`)
+.then((SearchResults)=>{
+const categoryResults = SearchResults[catogary]
+if(searchTerm){
+const results = categoryResults.filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()))
+setProducts(results)
+}
+
+})
+
+}
+
+
+useEffect(()=>{
+getSearchResults()
+},[searchParams])
   return (
     <div>SearchResults</div>
   )
